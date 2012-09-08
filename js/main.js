a6f302d84e55d133b65b6bba7d0fb63d21c3383b
Paul Venuto <pavenuto@gmail.com>
2012-09-08 10:17:23 -0400

    basic window info markup

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ stva.init = function(){
 	$('#map_canvas').gmap({'center': '37.5536117554, -77.4605636597', 'zoom': 11, 'disableDefaultUI':true, 'callback': function() {
 		var self = this;
 		self.addMarker({'position': this.get('map').getCenter() }).click(function() {
-			self.openInfoWindow({ 'content': 'Hello World!' }, this);
+			self.openInfoWindow({ 'content': '<h3 class="startup-name">Startup Name</h3><a class="url" href="#">www.domain.com</a>' }, this);
 		});
 	}});
 
