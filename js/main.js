4e2ee4a5b168d22e09b56bdc4ea61a13040a2d4a
Paul Venuto <pavenuto@gmail.com>
2012-09-08 12:21:32 -0400

    further refinement in UI, custom map display, typography

--- js/main.js
+++ js/main.js
@@ -1,5 +1,63 @@
 var stva = {};
 
+var mapStyles = [
+         {
+            featureType: "road",
+            elementType: "geometry",
+            stylers: [
+              { hue: "#ffb400" },
+              { lightness: 100 }
+            ]
+          },{
+            featureType: "road",
+            stylers: [
+              { visibility: "on" },
+              { hue: "#91ff00" },
+              { saturation: -62 },
+              { gamma: 1.98 },
+              { lightness: 45 }
+            ]
+          },{
+            featureType: "water",
+            stylers: [
+              { hue: "#005eff" },
+              { gamma: 0.72 },
+              { lightness: 42 }
+            ]
+          },{
+            featureType: "transit.line",
+            stylers: [
+              { visibility: "off" }
+            ]
+          },{
+            featureType: "administrative.locality",
+            stylers: [
+              { visibility: "on" }
+            ]
+          },{
+            featureType: "administrative.neighborhood",
+            elementType: "geometry",
+            stylers: [
+              { visibility: "simplified" }
+            ]
+          },{
+            featureType: "landscape",
+            stylers: [
+              { visibility: "on" },
+              { gamma: 0.41 },
+              { lightness: 46 }
+            ]
+          },{
+            featureType: "administrative.neighborhood",
+            elementType: "labels.text",
+            stylers: [
+              { visibility: "on" },
+              { saturation: 33 },
+              { lightness: 20 }
+            ]
+          }
+        ];
+
 $(function(){
 
 	stva.init();
@@ -9,11 +67,15 @@ $(function(){
 
 stva.init = function(){
 
-	$('#map_canvas').gmap({'center': '37.5536117554, -77.4605636597', 'zoom': 11, 'disableDefaultUI':true, 'callback': function() {
+	$('#map_canvas').gmap({'center': '37.5536117554, -77.4605636597', 'styles': mapStyles, 'zoom': 11, 'disableDefaultUI':true, 'callback': function() {
 		var self = this;
 		self.addMarker({'position': this.get('map').getCenter() }).click(function() {
 			self.openInfoWindow({ 'content': '<h3 class="startup-name">Startup Name</h3><a class="url" href="#">www.domain.com</a>' }, this);
 		});
 	}});
+
+	$(".list").find('a').click(function(e) {
+		e.preventDefault();
+	});
 
 };
