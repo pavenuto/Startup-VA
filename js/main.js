a61d0c05999e03fb94b8e8631a0b8715e7b2cb0b
Paul Venuto <pavenuto@gmail.com>
2012-09-08 13:49:39 -0400

    custom marker

--- js/main.js
+++ js/main.js
@@ -1,62 +1,62 @@
 var stva = {};
 
 var mapStyles = [
-         {
-            featureType: "road",
-            elementType: "geometry",
-            stylers: [
-              { hue: "#ffb400" },
-              { lightness: 100 }
-            ]
-          },{
-            featureType: "road",
-            stylers: [
-              { visibility: "on" },
-              { hue: "#91ff00" },
-              { saturation: -62 },
-              { gamma: 1.98 },
-              { lightness: 45 }
-            ]
-          },{
-            featureType: "water",
-            stylers: [
-              { hue: "#005eff" },
-              { gamma: 0.72 },
-              { lightness: 42 }
-            ]
-          },{
-            featureType: "transit.line",
-            stylers: [
-              { visibility: "off" }
-            ]
-          },{
-            featureType: "administrative.locality",
-            stylers: [
-              { visibility: "on" }
-            ]
-          },{
-            featureType: "administrative.neighborhood",
-            elementType: "geometry",
-            stylers: [
-              { visibility: "simplified" }
-            ]
-          },{
-            featureType: "landscape",
-            stylers: [
-              { visibility: "on" },
-              { gamma: 0.41 },
-              { lightness: 46 }
-            ]
-          },{
-            featureType: "administrative.neighborhood",
-            elementType: "labels.text",
-            stylers: [
-              { visibility: "on" },
-              { saturation: 33 },
-              { lightness: 20 }
-            ]
-          }
-        ];
+ {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      { hue: "#ffb400" },
+      { lightness: 100 }
+    ]
+  },{
+    featureType: "road",
+    stylers: [
+      { visibility: "on" },
+      { hue: "#91ff00" },
+      { saturation: -62 },
+      { gamma: 1.98 },
+      { lightness: 45 }
+    ]
+  },{
+    featureType: "water",
+    stylers: [
+      { hue: "#005eff" },
+      { gamma: 0.72 },
+      { lightness: 42 }
+    ]
+  },{
+    featureType: "transit.line",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },{
+    featureType: "administrative.locality",
+    stylers: [
+      { visibility: "on" }
+    ]
+  },{
+    featureType: "administrative.neighborhood",
+    elementType: "geometry",
+    stylers: [
+      { visibility: "simplified" }
+    ]
+  },{
+    featureType: "landscape",
+    stylers: [
+      { visibility: "on" },
+      { gamma: 0.41 },
+      { lightness: 46 }
+    ]
+  },{
+    featureType: "administrative.neighborhood",
+    elementType: "labels.text",
+    stylers: [
+      { visibility: "on" },
+      { saturation: 33 },
+      { lightness: 20 }
+    ]
+  }
+];
 
 $(function(){
 
@@ -64,18 +64,30 @@ $(function(){
 
 });
 
+stva.resize_sidebar = function(){
+
+	$(".list-wrap").height($(window).height() - $('.header').height());
+
+};
+
 
 stva.init = function(){
 
 	$('#map_canvas').gmap({'center': '37.5536117554, -77.4605636597', 'styles': mapStyles, 'zoom': 11, 'disableDefaultUI':true, 'callback': function() {
 		var self = this;
-		self.addMarker({'position': this.get('map').getCenter() }).click(function() {
+		self.addMarker({'position': this.get('map').getCenter(), 'icon': "http://localhost:8000/img/marker.png" }).click(function() {
 			self.openInfoWindow({ 'content': '<h3 class="startup-name">Startup Name</h3><a class="url" href="#">www.domain.com</a>' }, this);
 		});
 	}});
 
 	$(".list").find('a').click(function(e) {
 		e.preventDefault();
+	});
+
+	stva.resize_sidebar();
+
+	$(window).bind('resize',function(event) {
+		stva.resize_sidebar();
 	});
 
 };
