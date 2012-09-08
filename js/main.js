ab47ad09c592b08b48ede4fefef7e2de67125515
Paul Venuto <pavenuto@gmail.com>
2012-09-08 10:03:36 -0400

    setups initial map functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+var stva = {};
 
+$(function(){
+
+	stva.init();
+
+});
+
+
+stva.init = function(){
+
+	$('#map_canvas').gmap({'center': '37.5536117554, -77.4605636597', 'zoom': 11, 'disableDefaultUI':true, 'callback': function() {
+		var self = this;
+		self.addMarker({'position': this.get('map').getCenter() }).click(function() {
+			self.openInfoWindow({ 'content': 'Hello World!' }, this);
+		});
+	}});
+
+};
